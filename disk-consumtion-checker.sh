49ad523ca659e580a12e3c92613128517d575613
Abdul Wajid <abdulwajid609@gmail.com>
2025-05-01 06:21:29 +0500

    Create disk-consumtion-checker.sh
    init

---
+++ disk-consumtion-checker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for site in /etc/nginx/sites-available/*; do
+    app_name=$(basename "$site")
+    wp_path="/home/master/applications/$app_name/public_html/wp-content"
+
+    if [ -d "$wp_path" ]; then
+        echo "Disk usage for: $app_name (wp-content)"
+        echo "---------------------------------------"
+        (cd "$wp_path" && du -shc ./* 2>/dev/null)
+        echo ""
+    else
+        echo "wp-content not found for: $app_name"
+        echo ""
+    fi
+done
